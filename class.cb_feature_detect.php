b7ce36e8b43637ac35402661167f2a6eefc69360
Ulf Hermann <ulfhermann@culturebase.org>
2012-03-15 08:26:16 +0000

    coding style

--- class.cb_feature_detect.php
+++ class.cb_feature_detect.php
@@ -1,11 +1,13 @@
 <?php
 
 class CbFeatureDetect {
+
    protected $features = array();
 
-   public function run() {
+   public function run()
+   {
       if ($_SESSION['feature_detect_running']) {
-         foreach($_POST as $name => $value) {
+         foreach ($_POST as $name => $value) {
             if ($value === 'true') {
                $this->features[$name] = true;
             } else if ($value === 'false') {
@@ -19,16 +21,19 @@ class CbFeatureDetect {
          $_SESSION['feature_detect_running'] = false;
       } else {
          $_SESSION['feature_detect_running'] = true;
-         require dirname(__FILE__).'/templates/feature-detect.html';
+         require dirname(__FILE__) . '/templates/feature-detect.html';
          die();
       }
    }
 
-   public function get($feature) {
+   public function get($feature)
+   {
       return array_key_exists($feature, $this->features) ? $this->features[$feature] : false;
    }
 
-   public function getAll() {
+   public function getAll()
+   {
       return $this->features;
    }
+
 }
